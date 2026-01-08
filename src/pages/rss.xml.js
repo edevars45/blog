005e2a0bb89e26370c2d9ddc032bded88e8ef976
Esther <devarsesther@gmail.com>
2026-01-08 16:08:01 +0100

    Finalisation Unité 5 : Ajout des tags dynamiques, de la page d'index et du flux RSS

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,13 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: "Apprenti Astro | Blog",
+    description: "Mon parcours d'apprentissage d'Astro",
+    site: context.site,
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./posts/*.md'),
+    ),
+    customData: `<language>fr-fr</language>`,
+  });
+}
